475fd3becda12abec029e658bbce7b7e734569cd
inmovly <inmovly@gmail.com>
2024-11-21 16:01:31 +0100

    Create Product.js

---
+++ backend/models/Product.js
@@ -0,0 +1,10 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../config/database');
+
+const Product = sequelize.define('Product', {
+  name: { type: DataTypes.STRING, allowNull: false },
+  price: { type: DataTypes.FLOAT, allowNull: false },
+  image: { type: DataTypes.STRING, allowNull: true },
+});
+
+module.exports = Product;
